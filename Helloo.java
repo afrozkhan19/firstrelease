6c229dba2c572e96cf3fff0a06e43a5d32019959
afrozkhan19 <afroz.19.khan@gmail.com>
2025-02-11 16:32:34 +0530

    commit1

---
+++ Helloo.java
@@ -0,0 +1,19 @@
+
+      public class Helloo
+      {
+         public static void main(String args[])
+         {
+             int aa=10; int bb;
+              try{
+              bb=aa/0;
+System.out.println(bb);
+    System.out.println(args[0]);
+               }
+              catch(Exception e)
+               {
+System.out.println("pls provide proper values "+e);
+                }
+                           
+             
+          }
+       }
